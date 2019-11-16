cf33031bd9328544f6a67741418642ac6596b0f3
Kailai <48574336+KAILAI-Y@users.noreply.github.com>
2019-11-16 00:28:22 -0500

    motify PHP
    finish the PHP to check the email and password in Mysql and push out the welcome and  error page.

--- checkUser.php
+++ checkUser.php
@@ -1,18 +1,45 @@
 <?php
- $query="SELECT * FROM user WHERE email='$email'";
-        $result=mysqli_query($db, $query) or die(mysqli_error($db));
+$emailAddress = $_POST['email'];
+$userpassword =$_POST['password'];
 
-        if(mysqli_num_rows($result)!=0){
-            while ($row = mysqli_fetch_assoc($result)){
-                $dbpassword = $row['password'];
-            }
-            if(password_verify($password, $dbpassword)){
-                header("location: notes.php");
-            }else{
-                echo "Login error. ";
-            }
-        }else{
-            echo "Login error. ";
-        }
+$servername="localhost";
+$username = "root";
+$password = "";
+$dbname = "assignment3";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn-> connect_error){
+    die("connect error" . $conn->connect_error);
+}
+echo $emailAddress;
+$userpassword2 = null;
+
+$sql="SELECT Password FROM users where EmailAddress = '{$emailAddress}'";
+$result = $conn -> query($sql);
+
+if($result-> num_rows > 0){
+    $row = $result->fetch_assoc();
+   // $firstName = $row["FirstName"];
+   // $lastName = $row["LastName"];
+   // $username = $row["UserName"];
+   // $emailAddress = $row["EmailAddress"];
+   $userpassword2 = $row["Password"];    
+   // echo $firstName. "<br/>";
+   // echo $lastName. "<br/>";
+   // echo $username. "<br/>";
+   //  echo $emailAddress. "<br/>";
+   //echo $userpassword2. "<br/>";
+}
+
+
+$conn->close();
+
+if($userpassword == $userpassword2){
+    header("location:welcome.html");
+}else{
+    header("location:logerr.html");
+}
+
 
 ?>
